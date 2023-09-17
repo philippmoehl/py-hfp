78fb707fd3ba5b9bd7c1dcf2c1d9d69a7fbaacd9
philippmoehl <maxxphilipp@gmail.com>
2023-09-17 13:09:25 +0200

    adapt building script

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from glob import glob
 from pathlib import Path
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-__version__ = "0.1.0"
+__version__ = "0.1"
 
 source_path = Path(r"hfp/src").glob('**/*.cpp')
 
 
 ext_modules = [
         Pybind11Extension(
-            "hfp", 
+            "pyhfp.hfp", 
             sources=[
                 str(file.as_posix())
                 for file in source_path
@@ -22,14 +22,19 @@ ext_modules = [
     ]
 
 setup(
-    name="hfp",
+    name="pyhfp",
     version=__version__,
     author="Philipp Moehl",
     author_email="philipp1994@gmail.com",
     url="https://github.com/philippmoehl/py-hfp",
     description="Hierarchical Fitting Primitives.",
     long_description="",
+    packages=find_packages(),
     ext_modules=ext_modules,
+    install_requires=[
+        'numpy>=1.20.0',
+        'trimesh>=3.23.5'
+    ],
     extras_require={"test": "pytest"},
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
